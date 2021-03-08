853bea23913c3fef8cf242ba27eb95c8188f8b6b
StefanoS <iiostefanos@gmail.com>
2021-03-08 20:29:16 +0000

    added static root

--- pythonpswd-project/pythonpswd/settings.py
+++ pythonpswd-project/pythonpswd/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-
+import os
 from pathlib import Path
 
 
@@ -125,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
