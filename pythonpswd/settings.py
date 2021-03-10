4cf8708e82ddbf64147d4930091f3d09f988a27d
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-10 07:51:40 +0000

    Remove comment from settings.py.

--- pythonpswd/settings.py
+++ pythonpswd/settings.py
@@ -17,9 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve()
 
-print(BASE_DIR)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
